2d46503a872502a48be64d992ddb4c38b0fef7bd
Kevin Waugh <waughkev@gmail.com>
2015-08-26 12:26:19 -0400

    Adding first files

---
+++ Best.R
@@ -0,0 +1,10 @@
+best <- function(state, outcome) {
+  #data being made inside function
+  bestdata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+  ## Read outcome data
+  
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+}
