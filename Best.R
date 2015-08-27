7d4981bfcd116b3ec1bc59f8fe194984e7ba6b64
Kevin Waugh <waughkev@gmail.com>
2015-08-27 11:41:35 -0400

    Completed Best.R

--- Best.R
+++ Best.R
@@ -2,23 +2,34 @@ best <- function(state, outcome) {
   #data being made inside function
   bestdata <- data.frame
   bestdata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  #Subset of information based on state given
-  statedata <- subset(bestdata, bestdata$State==state)
-  ## Read outcome data
+  tolower(outcome)
+  toupper(state)
+  #Grab all state values and check against entry, throw error is needed.
+  allstates <- unique(bestdata$State)
+  #If statement for states
+  if (is.na(match(state, allstates))) {
+    stop("Invalid State")
+  } else { message("State is valid")}
   
   #Set column to read from based on outcome
-  if (outcome=="Heart attack") {
-    MortalityColumn <- statedata$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack
-  } else if (outcome=="Heart Failure") {
-    MortalityColumn <- statedata$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure
-  } else if (outcome=="Pneumonia") {
-    MortalityColumn <- statedata$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia
+  if (outcome=="heart attack") {
+    MortalityColumn <- 11
+  } else if (outcome=="heart failure") {
+    MortalityColumn <- 17
+  } else if (outcome=="pneumonia") {
+    MortalityColumn <- 23
   } else {
     #Throw error
-    stop("invalid state")
+    stop("Invalid Disease")
   }
-  #Grab all state values and check against entry, throw error is needed.
-  ## Check that state and outcome are valid
+  
+  #Subset of information based on state given
+  statedata <- subset(bestdata, bestdata$State==state, select = c(2,7, MortalityColumn))
+  na.omit(statedata)
+  statedata[,3] <- as.numeric(statedata[,3])
+  #Sort data by lowest 30-day death
+  statedata <- statedata[order(statedata[,3], statedata[,1]), ]
   ## Return hospital name in that state with lowest 30-day death
+  return(statedata[1,1])
   ## rate
 }
